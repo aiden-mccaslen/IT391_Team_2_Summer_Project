52da7b7ab5066934fe218d6702d8a5885ef82add
Aiden McCaslen <47439164+aiden-mccaslen@users.noreply.github.com>
2026-07-03 00:27:26 -0500

    Created Initial expense page
    We should redo the style later

--- Frontend/javascript/expenses.js
+++ Frontend/javascript/expenses.js
@@ -1,18 +1,79 @@
-function handleIn() {
-    const test = document.getElementById("test").value;
-    const testNum = parseFloat(test);
-    const feedback = document.getElementById("feedback");
-    if(isNaN(testNum) || !Number.isFinite(testNum)) {
-        feedback.textContent = "Please enter a proper exepense value.";
-        feedback.style.color = "red";
-    } else {
-        localStorage.setItem("testNum", testNum);
-        feedback.textContent = "Saved value: " + testNum;
-        feedback.style.color = "green";
-    }
+const expenseForm = document.getElementById("expenseForm");
+const log = document.querySelector("#log");
+
+const API_BASE_URL = "http://localhost:5500";
+
+console.log(expenseForm)
+
+if (expenseForm) {
+    expenseForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const data = new FormData(expenseForm);
+        console.log("data: "+data);
+        let amount;
+        let date;
+        let expenseType = "";
+        for(const entry of data){
+            console.log("entry: "+entry)
+
+            if(entry[0] == "amount"){
+                amount = entry[1];
+                // const amount = amountInput.value;
+                console.log(amount)
+            }
+
+            if(entry[0] == "date"){
+                date = entry[1];
+                console.log(date)
+            }
+
+            if(entry[0] == "expense"){
+                expenseType = entry[1];
+                console.log(expenseType)
+            }
+
+            
+        }
+        // testing
+        console.log(amount); 
+        console.log(date);
+        console.log(expenseType);
+        //backend package
+        const expenseData = {
+            amount: amount,
+            date: date,
+            expenseType: expenseType
+        };
+        // testing
+        console.log(expenseData);
+
+        sendLoginRequest(loginData);
+    });
+
 }
 
-function clearIn() {
-    document.getElementById("test").value="";
-    document.getElementById("feedback").textContent="";
+async function sendExpenseRequest(loginData) {
+    try {
+        const response = await fetch(`${API_BASE_URL}/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(expenseData)
+        });
+
+        const result = await response.json();
+
+        if (result.success) {
+            window.location.href = "../html/expenses.html";
+        }
+        else
+        {
+            alert(result.message);
+        }
+
+        console.log("Expense response:", result);
+    } catch (error) {
+        console.error("Expense request failed:", error);
+    }
 }
