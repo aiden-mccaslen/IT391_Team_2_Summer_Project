b6ae342c5ce987080a259f4a49105082b6687ab1
Shalom <shalomadiboshi01@gmail.com>
2026-08-04 00:25:37 -0500

    Main merge

---
+++ Backend/budget.py
@@ -0,0 +1,14 @@
+# Maps each expense category to one of the 50/30/20 budget groups.
+# Needs are food, clothing and shelter related stuff, all other things are wants
+# Might want to consider car payment and insurance as Needs
+CATEGORY_MAP = {
+    "Rent/Mortgage": "Need",
+    "Utilities": "Need",
+    "Groceries": "Need",
+
+    "Car Payment": "Want",
+    "Car Insurance": "Want",
+    "Streaming Services": "Want",
+    "Personal": "Want",
+    "Unexpected": "Want"
+}
